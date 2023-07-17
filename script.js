40e5a67e75acf3e854fa6289ecf24bd44a331a23
santiagokdena <scadenaa@unal.edu.co>
2023-07-17 12:37:52 -0500

    new commit

--- script.js
+++ script.js
@@ -6,3 +6,22 @@ for (var i = 0; i < scrollLinks.length; i++) {
     section.scrollIntoView({ behavior: "smooth" });
   });
 }
+const form=document.getElementById("form")
+async function handleSendEmail(event){
+  event.preventDefault();
+  const fd = new FormData(this)
+  const respone = await fetch('https://formspree.io/f/xjvqkvpn',{
+    method:'POST',
+    body: fd,
+    headers:{
+      Accept: 'application/json'
+    }
+  })
+  if(Response.ok){
+    this.reset()
+    alert('Mensaje enviado')
+  } else{
+    alert('Error al enviar el mensaje')
+  }
+}
+form.addEventListener('submit',handleSendEmail)
